455e226f3c300aee5d7c2698c063043831e4fdd2
d3ller <corentin.nelhomme@gmail.com>
2026-09-25 11:33:12 +0200

    build: enable React Compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import react from '@vitejs/plugin-react'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
 import { defineConfig } from 'vite'
 import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
   resolve: {
     alias: {
       '@ui': fileURLToPath(new URL('./src/components/ui', import.meta.url)),
